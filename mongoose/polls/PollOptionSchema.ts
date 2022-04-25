032996151878c4663a9ab4cfea31650e1b0a2e25
Devroop Kar <kardevroop@gmail.com>
2022-04-25 16:00:13 -0400

    Added dislikes functionality

--- mongoose/polls/PollOptionSchema.ts
+++ mongoose/polls/PollOptionSchema.ts
@@ -4,6 +4,6 @@ import PollOption from "../../models/polls/PollOption";
 const PollOptionSchema = new mongoose.Schema<PollOption>({
     optionText: {type: String, required: true},
     numVoted: {type: Number, default: 0},
-    tuit: {type: Schema.Types.ObjectId, ref: "TuitModel"}
+    tuit: {type: Schema.Types.ObjectId, ref: "TuitModel", required: true}
 }, {collection: "pollOptions"});
 export default PollOptionSchema;
